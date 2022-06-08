2110e6c1d9f953462530e221fa42ff9b6cf4108c
Aman Goswami <97969099+Galvan7@users.noreply.github.com>
2022-06-08 09:44:04 +0530

    Add files via upload

---
+++ 1SizeofDT.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int a; //declaration
+    a=45; //initiation
+
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of char "<<sizeof(c)<<endl;
+
+    return 0;
+}
